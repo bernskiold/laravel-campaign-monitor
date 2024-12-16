09534fd7e9bb92310d3ea8eb4007db7fced92ab4
Erik Bernskiold <erik@bernskioldmedia.com>
2024-12-16 22:33:20 +0100

    Add more endpoints

--- src/Api/CampaignMonitor.php
+++ src/Api/CampaignMonitor.php
@@ -2,9 +2,15 @@
 
 namespace BernskioldMedia\LaravelCampaignMonitor\Api;
 
+use CS_REST_Campaigns;
+use CS_REST_Clients;
 use CS_REST_General;
+use CS_REST_Journeys;
 use CS_REST_Lists;
+use CS_REST_Segments;
 use CS_REST_Subscribers;
+use CS_REST_Templates;
+use CS_REST_Transactional_SmartEmail;
 
 use function config;
 
@@ -29,13 +35,43 @@ class CampaignMonitor
         return new CS_REST_General($this->authentication);
     }
 
+    public function campaigns(?string $campaignId = null): CS_REST_Campaigns
+    {
+        return new CS_REST_Campaigns($campaignId, $this->authentication);
+    }
+
+    public function clients(?string $clientId = null): CS_REST_Clients
+    {
+        return new CS_REST_Clients($clientId, $this->authentication);
+    }
+
+    public function journeys(?string $journeyId = null): CS_REST_Journeys
+    {
+        return new CS_REST_Journeys($journeyId, $this->authentication);
+    }
+
     public function lists(?string $listId = null): CS_REST_Lists
     {
         return new CS_REST_Lists($listId, $this->authentication);
     }
 
+    public function segments(?string $segmentId = null): CS_REST_Segments
+    {
+        return new CS_REST_Segments($segmentId, $this->authentication);
+    }
+
     public function subscribers(string $listId): CS_REST_Subscribers
     {
         return new CS_REST_Subscribers($listId, $this->authentication);
+    }
+
+    public function templates(?string $templateId = null): CS_REST_Templates
+    {
+        return new CS_REST_Templates($templateId, $this->authentication);
+    }
+
+    public function transactional(?string $transactionalId = null): CS_REST_Transactional_SmartEmail
+    {
+        return new CS_REST_Transactional_SmartEmail($transactionalId, $this->authentication);
     }
 }
--- src/Facades/CampaignMonitor.php
+++ src/Facades/CampaignMonitor.php
@@ -2,15 +2,27 @@
 
 namespace BernskioldMedia\LaravelCampaignMonitor\Facades;
 
+use CS_REST_Campaigns;
+use CS_REST_Clients;
 use CS_REST_General;
+use CS_REST_Journeys;
 use CS_REST_Lists;
+use CS_REST_Segments;
 use CS_REST_Subscribers;
+use CS_REST_Templates;
+use CS_REST_Transactional_SmartEmail;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static CS_REST_General account()
+ * @method static CS_REST_Campaigns campaigns(?string $campaignId = null)
+ * @method static CS_REST_Clients clients(?string $clientId = null)
+ * @method static CS_REST_Journeys journeys(?string $journeyId = null)
  * @method static CS_REST_Lists lists(?string $listId = null)
+ * @method static CS_REST_Segments segments(?string $segmentId = null)
  * @method static CS_REST_Subscribers subscribers(string $listId)
+ * @method static CS_REST_Templates templates(?string $templateId = null)
+ * @method static CS_REST_Transactional_SmartEmail transactional(?string $transactionalId = null)
  * @method static bool isActive()
  */
 class CampaignMonitor extends Facade
